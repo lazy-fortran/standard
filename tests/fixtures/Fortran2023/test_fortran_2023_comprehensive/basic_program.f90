d3500519ca44ec6ec5e695b93bffb0744334402c
Christopher Albert <albert@tugraz.at>
2025-12-06 22:09:46 +0100

    Issue #100: move Fortran2023 comprehensive tests to fixtures

---
+++ tests/fixtures/Fortran2023/test_fortran_2023_comprehensive/basic_program.f90
@@ -0,0 +1,5 @@
+program test_f2023
+    integer :: x
+    x = 42
+end program test_f2023
+
---
+++ tests/fixtures/Fortran2023/test_fortran_2023_comprehensive/mixed_era_program.f90
@@ -0,0 +1,23 @@
+program historical_test
+    ! F77 CHARACTER type
+    character*20 :: name
+
+    ! F90 modules and allocation
+    use some_module, only: utility_func
+    integer, allocatable :: dynamic_array(:)
+
+    ! F2003 OOP
+    class(base_type), allocatable :: obj
+
+    ! F2008 coarrays
+    integer :: coarray[*]
+
+    ! F2018 collective operations
+    call co_sum(coarray)
+
+    ! F2023 conditional expressions
+    integer :: x, result
+    x = 1
+    result = x > 0 ? x : 0
+end program historical_test
+
