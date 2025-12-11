c7a1297eacb4ef0aad226fc011222c2b1f090659
Christopher Albert <albert@tugraz.at>
2025-12-11 13:26:28 +0100

    fix: parse format slash separators (fixes #596)

---
+++ tests/fixtures/FORTRAN/test_fortran_parser/format_slash_record_separators.f
@@ -0,0 +1,6 @@
+! 2025 FORMAT slash separators for IBM 704 record control
+      100  FORMAT(I5/F10.2)
+      200  FORMAT(5HNAME:, A6/5HAGE:, I3)
+      300  FORMAT(I5//F10.2)
+      400  FORMAT(10HRESULTS://3F10.2)
+      END
